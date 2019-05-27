2ec10759fee0d436a1bfcfebdc95873abfc504a1
Valentin Condrea <condrea.valentin@gmail.com>
2019-05-27 18:45:28 +0300

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Valentin Condrea on 27/05/2019.
+//  Copyright © 2019 Trainer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
